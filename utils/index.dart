a13af13ae9d7d2a5a162e0c855a03e51a007b364
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-12-28 01:22:16 +0100

    add quiver package

--- utils/index.dart
+++ utils/index.dart
@@ -1,3 +1,11 @@
 export 'generic_day.dart';
 export 'input_util.dart';
 export 'parse_util.dart';
+export 'field.dart';
+
+// package auto imports
+export 'package:collection/collection.dart';
+export 'package:tuple/tuple.dart';
+export 'package:quiver/core.dart';
+export 'package:quiver/iterables.dart';
+export 'package:quiver/strings.dart';
