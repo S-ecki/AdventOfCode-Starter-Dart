b3dba0b1ffe4cf2a81263c0b33a0fe814a5aba85
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-01 12:18:45 +0100

    added helper and better read input list

--- main.dart
+++ main.dart
@@ -1,17 +1,46 @@
 import 'solutions/index.dart';
 import 'utils/generic_day.dart';
 
-/// Const to switch between showing the results for each day in [days], or only
-/// the latest.
-const ONLY_SHOW_LAST = false;
-
 /// List holding all the solution classes.
 final days = <GenericDay>[
   Day01(),
 ];
 
-void main() {
-  ONLY_SHOW_LAST
+void main(List<String?> args) {
+  bool onlyShowLast = false;
+
+  if (args.length == 1 && args[0].isHelperArgument()) {
+    printHelper();
+    return;
+  }
+
+  if (args.length == 1 && args[0].isLastArgument()) {
+    onlyShowLast = true;
+  }
+
+  onlyShowLast
       ? days.last.printSolutions()
       : days.forEach((day) => day.printSolutions());
+}
+
+void printHelper() {
+  print(
+    '''
+Usage: dart main.dart <command>
+
+Global Options:
+  -h, --help    Show this help message
+  -l, --last    Only show last day
+''',
+  );
+}
+
+extension ArgsMatcher on String? {
+  bool isHelperArgument() {
+    return this == '-h' || this == '--help';
+  }
+
+  bool isLastArgument() {
+    return this == '-l' || this == '--last';
+  }
 }
--- utils/input_util.dart
+++ utils/input_util.dart
@@ -4,25 +4,38 @@ import 'dart:io';
 /// Note that file name and location must align.
 class InputUtil {
   final String _inputAsString;
+  final List<String> _inputAsList;
 
-  InputUtil(int day) : _inputAsString = _readInputDay(day);
+  InputUtil(int day)
+      : _inputAsString = _readInputDay(day),
+        _inputAsList = _readInputDayAsList(day);
+
+  static String _createInputPath(int day) {
+    String dayString = day.toString().padLeft(2, '0');
+    return './input/aoc$dayString.txt';
+  }
 
   static String _readInputDay(int day) {
-    String dayString = day.toString().padLeft(2, '0');
-    return _readInput('./input/aoc$dayString.txt');
+    return _readInput(_createInputPath(day));
   }
 
   static String _readInput(String input) {
     return File(input).readAsStringSync();
   }
 
+  static List<String> _readInputDayAsList(int day) {
+    return _readInputAsList(_createInputPath(day));
+  }
+
+  static List<String> _readInputAsList(String input) {
+    return File(input).readAsLinesSync();
+  }
+
   /// Returns input as one String.
   String get asString => _inputAsString;
 
-  /// Splits the input String by `newline`.
-  List<String> getPerLine() {
-    return _inputAsString.split('\n');
-  }
+  /// Reads the entire input contents as lines of text.
+  List<String> getPerLine() => _inputAsList;
 
   /// Splits the input String by `whitespace` and `newline`.
   List<String> getPerWhitespace() {
