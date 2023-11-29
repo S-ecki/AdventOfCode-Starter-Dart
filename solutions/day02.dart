138a10aae83b88b7fd1eded8f56f47bcb0fce834
WarriorsSami <sbarbutdica@gmail.com>
2023-11-29 11:41:25 +0200

    build: upgrade dart version

---
+++ solutions/day02.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day02 extends GenericDay {
+  Day02() : super(2);
+
+  @override
+  parseInput() {
+    
+  }
+
+  @override
+  int solvePart1() {
+    
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+
+    return 0;
+  }
+}
+
